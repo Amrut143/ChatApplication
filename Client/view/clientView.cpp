04f6e08206fcc4f4c25d453f4b8891c127cbeb11
saurabh chopade <saurabhchopade3250@gmail.com>
2020-10-08 12:08:45 +0530

    ADDED: Code to display view to the client

---
+++ Client/view/clientView.cpp
@@ -0,0 +1,8 @@
+#include "clientView.h"
+
+void ClientView::displayMesseges() {
+    cout << "Socket creation error";
+    cout << "Invalid address/ Address not supported";
+    cout << "Connection Failed";
+    cout << "Connected with server. Sending data"; 
+}
