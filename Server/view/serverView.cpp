a1e0e72f7944d44edc0cb2035e37f1cae5ed0e51
Amrutnandan Panda <amrutpanda7789@gmail.com>
2020-10-08 10:57:31 +0530

    ADDED: Code to view message after connection established

---
+++ Server/view/serverView.cpp
@@ -0,0 +1,9 @@
+#include "serverView.h"
+
+void ServerView::displayMsgToConnectedClients()
+{
+    cout << "*****Welcome to my chat application*****" << endl; 
+    cout << "Server started. Listening...." << endl;
+    cout << "Server started. Accepting...." << endl;
+    cout << "Connection Accepted. Reading....." << endl;
+}
